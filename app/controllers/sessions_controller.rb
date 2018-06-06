356aaa43e2a8b1a32e88ef83ce3b2982d2efc1d9
Sean Oughton <seanoughton@gmail.com>
2018-06-06 16:58:39 -0700

    fixed conflict between has_secure_password and omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,18 +10,21 @@ class SessionsController < ApplicationController
         u.user_name = auth['info']['name']
         u.email = auth['info']['email']
         u.image = auth['info']['image']
-        u.password = "password"
+        u.password = SecureRandom.urlsafe_base64
       end
     else #if logging in directly
         @user = User.find_by(user_name: params[:user][:user_name])
         @user = @user.try(:authenticate, params[:user][:password])
         #will make @user nil if the user cannot be authenticated with the username and password
         #try will return nil if it cannot authenticate, and then make @user nil
-        redirect_to(controller: 'sessions', action: 'new') unless @user
     end
-    session[:user_id] = @user.id #logs the user in
-    redirect_to(controller: 'welcome', action: 'home')
-    #render 'welcome/home'
+    if !@user
+      redirect_to(controller: 'sessions', action: 'new') #unless @user
+    else
+      session[:user_id] = @user.id #logs the user in
+      redirect_to(controller: 'welcome', action: 'home')
+    end
+
   end
 
   def destroy
