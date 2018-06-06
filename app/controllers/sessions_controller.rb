d7700811bc9ee9343ca4f154111c3926e8fb1a02
Sean Oughton <seanoughton@gmail.com>
2018-06-06 10:22:38 -0700

    added logic to login with either facebook or username/password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,33 +5,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.user_name = auth['info']['name']
-      u.email = auth['info']['email']
-      u.image = auth['info']['image']
+    if params[:code] #if logging in with facebook
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.user_name = auth['info']['name']
+        u.email = auth['info']['email']
+        u.image = auth['info']['image']
+      end
+    else #if logging in directly
+        @user = User.find_by(user_name: params[:user][:user_name])
+        @user = @user.try(:authenticate, params[:user][:password])
+        #will make @user nil if the user cannot be authenticated with the username and password
+        #try will return nil if it cannot authenticate, and then make @user nil
+        redirect_to(controller: 'sessions', action: 'new') unless @user
     end
-
-    session[:user_id] = @user.id
-
-    render 'welcome/home'
+    session[:user_id] = @user.id #logs the user in
+    redirect_to(controller: 'welcome', action: 'home')
+    #render 'welcome/home'
   end
 
-
-=begin
-  def create #login directly
-    user = User.find_by(user_name: params[:user][:user_name])
-    user = user.try(:authenticate, params[:user][:password])
-    #will make @user nil if the user cannot be authenticated with the username and password
-    #try will return nil if it cannot authenticate, and then make @user nil
-
-    return redirect_to(controller: 'sessions', action: 'new') unless user
-    #return head(:forbidden) unless @user.authenticate(params[:user][:password])
-    session[:user_id] = user.id #logs user in
-    @user = user
-    redirect_to controller: 'welcome', action: 'home'
-  end
-=end
   def destroy
     session.delete :user_id
     redirect_to '/'
